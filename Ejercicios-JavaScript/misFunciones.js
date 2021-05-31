5ec8d5636d93ac1532c243cec97cd25a33ce3b30
edme88 <edmealiciardi@gmail.com>
2021-05-30 21:09:28 -0300

    Se agregaron ejercicios JS

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -17,6 +17,10 @@
 function conversor(id_input, valor_input) {
     var val_metros, val_pulgadas, val_yardas, val_pies;
 
+    if(valor_input.includes(",")){
+        valor_input = valor_input.replace(",", ".");
+    }
+
     if(isNaN(valor_input)){
         val_metros = "";
         val_pies = "";
@@ -28,12 +32,10 @@ function conversor(id_input, valor_input) {
         val_pulgadas=39.3701*valor_input;
         val_yardas=1.09361*valor_input;
     }
-    document.getElementById("metro").value= val_metros;
-    document.form[0].input[0].value  = val_metros;
-    document.lasUnidades.unid_metro.value = val_metros;
-    document.getElementById("pie").value= val_pies;
-    document.getElementById("pulgada").value=val_pulgadas;
-    document.getElementById("yarda").value=val_yardas;
+    document.getElementById("metro").value= Number(val_metros).toFixed(2);
+    document.getElementById("pie").value= Number(val_pies).toFixed(2);
+    document.getElementById("pulgada").value=Number(val_pulgadas).toFixed(2);
+    document.getElementById("yarda").value=Math.round(val_yardas*100)/100;
 }
 
 
@@ -57,4 +59,64 @@ function convertirRG() {
     radianes = document.getElementById("radianes").value;
     grados = (radianes*180)/Math.PI;
     document.getElementById("grados").value = grados;
+}
+
+function mostrar_ocultar(nombre) {
+    if(nombre=="val_mostrar"){
+        document.getElementById("unDiv").style.display = 'block';
+    }else if(nombre=="val_ocultar"){
+        document.getElementById("unDiv").style.display = 'none';
+    }
+}
+
+function calcularSuma() {
+    var num1, num2;
+
+    num1 = Number(document.getElementById("nums1").value);
+    num2 = document.getElementById("nums2").value;
+
+    document.getElementById("totalS").innerHTML = num1+Number(num2);
+}
+
+function cargarWeb() {
+    var cant, unidad, urlComp;
+
+    cant = document.getElementById("distancia").value;
+    unidad = document.getElementsByName("unidades")[0].value;
+
+    urlComp= "segundaWeb.html#"+cant+"#"+unidad;
+    window.open(urlComp);
+}
+
+/**
+ * Carga los resultados en la segunda Web de valor y unidad
+ * @method cargarResultado
+ */
+function cargarResultado() {
+    var urlComp, cant, un;
+
+    urlComp=window.location.href.split("/");
+    urlComp=urlComp[urlComp.length-1];
+
+    cant=urlComp.split("#")[1];
+    un=urlComp.split("#")[2];
+
+    document.getElementById("dist").value = cant + " " + un;
+}
+
+function dibujarCirculoCuadrado() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var xMax, yMax, margen;
+    xMax = canvas.width;
+    yMax = canvas.height;
+    margen=10;
+
+    ctx.fillStyle = "#333899";
+    ctx.fillRect(margen,yMax-50-margen,50,50);
+
+    ctx.fillStyle = "#ff8627";
+    ctx.arc(xMax/2, yMax/2, 25, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.fill();
 }
